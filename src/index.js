acbee9c9c938a0b429b124ebad8d9b3f3ee7bb9e
Yuri0494 <76704347+Yuri0494@users.noreply.github.com>
2021-08-16 22:36:30 +0300

    init: get function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function toReadable (number) {
-  
+    let res = ""
+  let a = [
+    '', 'one', 'two', 'three', 'four',
+    'five', 'six', 'seven', 'eight', 'nine',
+    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
+    'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+  ];
+  let b = [
+    '', '', 'twenty', 'thirty', 'forty',
+    'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+  ];
+  let g = [
+    '', 'thousand', 'million', 'billion', 'trillion', 'quadrillion',
+    'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion'
+  ];
+
+  if(number === 0) {
+        return "zero"
+  }
+
+  if(number < 20) {
+        return a[number]
+  }
+
+  if(number >= 20 && number < 100) {
+        return b[(number - number%10) /10] + " " + a[number%10]
+  }
+
+  if(number >= 100 && number < 1000) {
+        res = a[(number - number%100) /100] + " hundred ";
+        if((number%100) < 20) {
+            return res + a[number%100]
+        } else {
+            return res + b[(number%100 - (number%100)%10) / 10] + " " + a[number%10]
+        }
+  }   
+   
 }
